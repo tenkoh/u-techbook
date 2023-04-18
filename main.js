07f7f197964070353bcb451ee39000bdaa9d5b3c
tenkoh <tenkoh.go@gmail.com>
2023-04-18 14:21:29 +0900

    mercari: fix link to mercari

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ function updateLinks() {
   const amazonLink = `https://www.amazon.co.jp/s?k=${
     encodeURIComponent(keyword)
   }&i=stripbooks&bbn=2761990051&rh=n%3A2761990051%2Cn%3A465392%2Cn%3A466298&dc&qid=1681794377&rnid=2761990051`;
-  const mercariLink = "https://www.mercari.com/jp/search/?keyword=" +
-    encodeURIComponent(keyword) + "&category_root=5&category_child=0";
+  const mercariLink = `https://jp.mercari.com/search?keyword=${
+    encodeURIComponent(keyword)
+  }&status=on_sale&category_id=674`;
   const paypayLink = "https://paypaymall.yahoo.co.jp/search?p=" +
     encodeURIComponent(keyword) + "&cid=465";
 
