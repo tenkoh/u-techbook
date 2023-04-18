c995e65b8a93cdb5eeef7eba3bae64268e54d1cd
tenkoh <tenkoh.go@gmail.com>
2023-04-18 14:30:37 +0900

    refactor: open links in new tab, remove duplicated operations

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ const searchInput = document.getElementById("searchInput");
 searchInput.addEventListener("input", updateLinks);
 
 function updateLinks() {
-  const keyword = searchInput.value;
+  const keyword = encodeURIComponent(searchInput.value);
 
-  const amazonLink = `https://www.amazon.co.jp/s?k=${
-    encodeURIComponent(keyword)
-  }&i=stripbooks&bbn=2761990051&rh=n%3A2761990051%2Cn%3A465392%2Cn%3A466298&dc&qid=1681794377&rnid=2761990051`;
-  const mercariLink = `https://jp.mercari.com/search?keyword=${
-    encodeURIComponent(keyword)
-  }&status=on_sale&category_id=674`;
-  const paypayLink = `https://paypayfleamarket.yahoo.co.jp/search/${
-    encodeURIComponent(keyword)
-  }?categoryIds=10002%2C10604&open=1`;
+  const amazonLink =
+    `https://www.amazon.co.jp/s?k=${keyword}&i=stripbooks&bbn=2761990051&rh=n%3A2761990051%2Cn%3A465392%2Cn%3A466298&dc&qid=1681794377&rnid=2761990051`;
+  const mercariLink =
+    `https://jp.mercari.com/search?keyword=${keyword}&status=on_sale&category_id=674`;
+  const paypayLink =
+    `https://paypayfleamarket.yahoo.co.jp/search/${keyword}?categoryIds=10002%2C10604&open=1`;
 
   document.getElementById("amazonLink").href = amazonLink;
   document.getElementById("mercariLink").href = mercariLink;
